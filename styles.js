28e142b9b4e1b21c8e286e3c315dbe51074180c2
thiernohbah <thiernobah2001@gmail.com>
2025-11-09 12:30:40 -0500

    task 1 complete

--- styles.js
+++ styles.js
@@ -1,1 +1,11 @@
-// this is my array assignment
+// this is my array assignmentcon
+const shoppingList =[];  //created empty array
+function addItem(items){
+    shoppingList.push(items)
+}
+function removeLastItem(items){
+    shoppingList.pop(items)
+}
+function displayList(items){
+    console.log("shoppingList:")
+}
